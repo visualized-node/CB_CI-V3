dcd6fdf08a89a7c4753a53947aa26d7c7c0bb218
visualized_node <gigachinned@gmail.com>
2023-04-13 17:48:14 +0200

    Updated settings to match new group settings that regard permissiongroups and the internalutils module

--- src/shared/InternalUtils.lua
+++ src/shared/InternalUtils.lua
@@ -1,11 +1,26 @@
 local Utils = {}
 local PermissionGroups = require(script.Parent.PermissionGroups)
 local Errors = require(script.Parent.types.Errors)
+local Settings = require(script.Parent.Settings)
 
 function Utils.GetPermissionGroupFromUserId(UserId: number)
+    local Player: Player = game.Players:GetPlayerFromUserId(UserId)
+
     for i, v in ipairs(PermissionGroups) do
         if table.find(v.Holders, UserId) then
             return v 
+        end
+
+        -- I could've shortened this but there are like 3 checks to be made xd
+
+        if v.IfGroupUsesRank then
+            if Settings.RankInclusion and Player:GetRankInGroup(Settings.GroupId) >= v.Identifier or Player:GetRankInGroup(Settings.GroupId) > v.Identifier then
+                return v
+            end
+        else
+            if Player:GetRoleInGroup(Settings.GroupId) == v.Identifier then
+                return v
+            end
         end
     end
 
--- src/shared/Settings.lua
+++ src/shared/Settings.lua
@@ -1,6 +1,6 @@
 local Settings = {
     -- Important
-    
+
     Prefix = "/", -- Defines the Prefix of a command (ex. /kick)
     ArgumentSeparator = " ", -- Defines the character used to separate arguments in a command (ex. Sep = ".", /kick.Hello.World)
     HasAccessToStore = false, -- If the module has access to the DataStoreService service in roblox
@@ -10,6 +10,7 @@ local Settings = {
 
     PermissionGroupsUseGroup = false,
     GroupId = 0, -- 0 if the value above is false
+    RankInclusion = true, -- defines whether Rank checks use >= (true) or > (false). Inclusion means that it includes the identifier of the permissiongroup if it's a rank.
 
     -- Permissions
 
